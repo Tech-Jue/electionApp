7a62dd284fc50a0dc13ddf3f767d4dec5e7325ae
leelos.js <leelahyeeson@gmail.com>
2024-08-05 13:27:57 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,43 +1,41 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(path.join(__dirname, "public"))) 
 
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database(':memory:');
+app.use(express.json()); // To parse JSON bodies
 
-db.serialize(() => {
-    db.run("CREATE TABLE lorem (info TEXT)");
+// Set the view engine to ejs
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
-    const stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-    for (let i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i);
-    }
-    stmt.finalize();
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-    db.each("SELECT rowid AS id, info FROM lorem", (err, row) => {
-        console.log(row.id + ": " + row.info);
-    });
+// Routes
+app.get('/', (req, res) => {
+    res.render('index'); // Render the index.ejs template
 });
 
-db.close();
+app.get('/home', (req, res) => {
+    res.render('index'); // Render the index.ejs template
+});
 
-app.get('/', (req,  res) =>  {
-
-    res.send("send file ")
-
-}    ) 
-
-app.get('/home', (req,  res) =>  {
-
-    res.sendFile(path.join(__dirname, "pudlic", "index.html"))
-
-})            
-
-
-
-console.log(path.join(__dirname, "public" ));
-app.listen(3000, () => {
-
-    console.log("server is listening on port 3000")
+app.post('/voter-registration', (req, res) => {
+    console.log(req.body)
 })
+
+app.get('/voter-registration', (req, res) => {
+    res.render('voter-registration.ejs')
+})
+
+// // Endpoint to handle form submission
+// app.post('/api/register', (req, res) => {
+//     const { firstName, middleName, lastName, email, password } = req.body;
+//     // Here, you would typically save the user data to a database
+//     console.log('User registered:', { firstName, middleName, lastName, email, password });
+//     res.json({ message: 'User registered successfully' });
+// });
+
+app.listen(3000, () => {
+    console.log('Server is listening on port 3000');
+});
